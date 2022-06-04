0bdefe820472d389ed15b82d2ff52ed5dd2b0fe9
mcelam00 <mcelam00@estudiantes.unileon.es>
2022-06-04 17:29:15 +0200

    Subtotal dinamico, cantidades y eliminar de cesta

--- java/controller/CestaController.java
+++ java/controller/CestaController.java
@@ -5,9 +5,19 @@
  */
 package controller;
 
+import EJB.CestaFacadeLocal;
+import EJB.ClientesFacadeLocal;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import modelo.Cesta;
+import modelo.Clientes;
+import modelo.Usuarios;
 
 /**
  *
@@ -18,12 +28,72 @@ import javax.inject.Named;
 public class CestaController implements Serializable{
     
     
+    private Usuarios usuarioActivo;
+    private Clientes infoEspCliente;
+    private List<Cesta> cestaUsuarioActivo;
+    
+    private float subtotal;
     
     
+    @EJB
+    private CestaFacadeLocal cestaEJB;
+       @EJB
+    private ClientesFacadeLocal clientesEJB;
+
+    public List<Cesta> getCestaUsuarioActivo() {
+        return cestaUsuarioActivo;
+    }
+
+    public void setCestaUsuarioActivo(List<Cesta> cestaUsuarioActivo) {
+        this.cestaUsuarioActivo = cestaUsuarioActivo;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+
+   
+       
     
     
+    @PostConstruct
+    public void init() {
+        
+        //Según el usuario que esté loggeado se trae lo que tenga en la cesta
+       this.usuarioActivo = (Usuarios)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioLoggeado");
+       this.infoEspCliente = this.clientesEJB.find(this.usuarioActivo.getEmail());
+              
+       this.cestaUsuarioActivo = this.cestaEJB.getProductos(this.infoEspCliente);
+       
+       this.calcularSubtotal();
+       
+    }
+
+    public void calcularSubtotal() {
+
+        float val = 0;
+        
+        for (int i = 0; i < this.cestaUsuarioActivo.size(); i++) {
+            val = val + (this.cestaUsuarioActivo.get(i).getUPC().getPriceNumeral() * this.cestaUsuarioActivo.get(i).getCantidad());           
+            
+        }      
+         
+
+        this.subtotal = val;
+    }
     
     
+    public void eliminarDeCesta(Cesta cestaABorrar){
+        //se elimina de la cesta de la base de datos y de la lista local
+        cestaEJB.remove(cestaABorrar);
+        System.out.println(this.cestaUsuarioActivo.size());                
+        this.cestaUsuarioActivo.remove(cestaABorrar);
+System.out.println(this.cestaUsuarioActivo.size());
+    }
     
     
     
