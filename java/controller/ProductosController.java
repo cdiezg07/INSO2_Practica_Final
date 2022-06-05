288cf62fbc70fd81069d44d7476e35cac9cd9d9b
mcelam00 <mcelam00@estudiantes.unileon.es>
2022-06-05 11:12:05 +0200

    opiniones en los productos

--- java/controller/ProductosController.java
+++ java/controller/ProductosController.java
@@ -38,20 +38,20 @@ import modelo.Valoracion;
  */
 @Named
 @RequestScoped
-public class ProductosController implements Serializable{
-    
+public class ProductosController implements Serializable {
+
     private Products producto;
     private List<Products> listaProductos;
-    
+
     @Inject
     private SubcategoriasController vistaAnterior;
-    
+
     @EJB
     private ProductsFacadeLocal productsEJB;
-    
+
     @EJB
     private OpinionesFacadeLocal opinionesEJB;
-     @EJB
+    @EJB
     private CestaFacadeLocal cestaEJB;
     @EJB
     private ClientesFacadeLocal clienteEJB;
@@ -63,117 +63,151 @@ public class ProductosController implements Serializable{
     private Usuarios usu;
     private Products productoSeleccionado;
     private boolean enElCarrito;
-    
+    private String nuevaResenya;
+    private int rating;
+
     @PostConstruct
-    public void init(){
-        
+    public void init() {
+
         producto = new Products();
-        usu = (Usuarios)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioLoggeado");
+        usu = (Usuarios) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuarioLoggeado");
         //this.productoSeleccionado = vistaAnterior.getActualProductos();
-                
+
         this.productoSeleccionado = this.productsEJB.find(FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("produtoSeleccionado"));
-        System.out.println("vista anterior congelada: "+this.vistaAnterior);
-        System.out.println("categoriaAnterior: "+this.productoSeleccionado.getName());
-        
-        this.enElCarrito = this.cestaEJB.yaExisteProducto(this.productoSeleccionado, clienteEJB.find(usu.getEmail()));
-        
+        System.out.println("vista anterior congelada: " + this.vistaAnterior);
+        System.out.println("categoriaAnterior: " + this.productoSeleccionado.getName());
+
+        if (usu != null) {
+            this.enElCarrito = this.cestaEJB.yaExisteProducto(this.productoSeleccionado, clienteEJB.find(usu.getEmail()));
+        } else {
+            this.enElCarrito = false;
+        }
+
     }
-        
-    public void insertarSubcategoria(){
-        try{
+
+    public void insertarSubcategoria() {
+        try {
             productsEJB.create(producto);
-        }catch(Exception e){
-            System.out.println("Error al insertar el producto "+ e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error al insertar el producto " + e.getMessage());
         }
     }
-    
-    public List<Products> getProducts(String nombreSubCategoria){
-       
 
-        try{
+    public List<Products> getProducts(String nombreSubCategoria) {
+
+        try {
             listaProductos = productsEJB.findAll();
             List<Products> listaFinal = new ArrayList<Products>();
-            System.out.println("fsdfasdfasdf"+nombreSubCategoria);
-            for(Products pro: listaProductos){
+            System.out.println("fsdfasdfasdf" + nombreSubCategoria);
+            for (Products pro : listaProductos) {
                 System.out.println(pro.getSubcategoria().getNombreSubcategoria());
-                if(pro.getSubcategoria().getNombreSubcategoria().equals(nombreSubCategoria)){
+                if (pro.getSubcategoria().getNombreSubcategoria().equals(nombreSubCategoria)) {
                     listaFinal.add(pro);
                 }
             }
             return listaFinal;
-            
-        }catch(Exception e){
-            System.out.println("Error al obtener el producto "+ e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println("Error al obtener el producto " + e.getMessage());
         }
         return null;
     }
-    
-    public void crearOpinion(){
+
+    public void crearOpinion() {
         Products producto;
-        if(usu.getEmail()==null){
+        if (usu.getEmail() == null) {
             System.out.println("No hay usuario logueado");
-        }else{
+        } else {
             cliente = clienteEJB.find(usu.getEmail());
             opinion.setComentario("Es la leche este producto");
             opinion.setEmailCliente(cliente);
             opinion.setFecha(new Date("07/07/2000"));
             opinion.setPuntuacion(0);
 //            opinion.setUPC(producto);
-            try{
-                 opinionesEJB.create(opinion);
-            }catch(Exception e){
-                System.out.println("Error al insertar el producto "+ e.getMessage());
+            try {
+                opinionesEJB.create(opinion);
+            } catch (Exception e) {
+                System.out.println("Error al insertar el producto " + e.getMessage());
             }
-            
+
         }
-        
+
     }
-    
-    public void crearValoracion(){
+
+    public void crearValoracion() {
         Products producto;
-        if(usu.getEmail()==null){
+        if (usu.getEmail() == null) {
             System.out.println("No hay usuario logueado");
-        }else{
+        } else {
             cliente = clienteEJB.find(usu.getEmail());
             valoracion.setEmailCliente(cliente);
             valoracion.setEstrellas(3);
 //          valoracion.setUPC(producto);
-            try{
+            try {
                 valoracionEJB.create(valoracion);
-            }catch(Exception e){
-                System.out.println("Error al insertar el producto "+ e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error al insertar el producto " + e.getMessage());
             }
-            
-        }
-        
-    }
-    
-    public void addCarrito(Products producto){
-        if(usu == null){
-                        addMessageError("Error", "Debe iniciar sesión para poder añadir productos a su carrito");            
 
-        }else{
-            System.out.println("Usuario: "+usu);
-            
+        }
+
+    }
+
+    public void addCarrito(Products producto) {
+        if (usu == null) {
+            addMessageError("Error", "Debe iniciar sesión para poder añadir productos a su carrito");
+
+        } else {
+            System.out.println("Usuario: " + usu);
+
             Cesta itemCesta = new Cesta();
             itemCesta.setUPC(this.productoSeleccionado);
             itemCesta.setCantidad(1);
             Clientes cli = clienteEJB.find(usu.getEmail());
 
             itemCesta.setEmailUsuario(cli);
-            try{
-            this.cestaEJB.create(itemCesta);
-            this.enElCarrito = true;
-            }catch(Exception e){
+            try {
+                this.cestaEJB.create(itemCesta);
+                this.enElCarrito = true;
+            } catch (Exception e) {
                 System.out.println("ERROR al insertar el producto en la cesta");
-            }            
+            }
         }
-              
+
     }
-    
-    public List<Opiniones> getOpiniones(Products producto){
+
+    public List<Opiniones> getOpiniones(Products producto) {
         List<Opiniones> opinion = productsEJB.getOpiniones(producto);
+      
+
         return opinion;
+    }
+
+    public void nuevaResenya() {
+
+        Opiniones nuevaOpinion = new Opiniones();
+        if (usu == null) {
+            System.out.println("No hay usuario logueado");
+            addMessageError("Error", "Debe iniciar sesión para poder comentar");
+
+        } else {
+            nuevaOpinion.setUPC(this.productoSeleccionado);
+            nuevaOpinion.setEmailCliente(clienteEJB.find(usu.getEmail()));
+            Date today = new Date();
+            today.setHours(0);
+            nuevaOpinion.setFecha(today);
+            nuevaOpinion.setComentario(this.nuevaResenya);
+            nuevaOpinion.setPuntuacion(this.rating);
+
+            try {
+
+                opinionesEJB.create(nuevaOpinion);
+                this.nuevaResenya = "";
+            } catch (Exception e) {
+                System.out.println("Error al insertar el producto " + e.getMessage());
+            }
+        }
+
     }
 
 //    public Categorias getCat() {
@@ -183,7 +217,6 @@ public class ProductosController implements Serializable{
 //    public void setCat(Categorias cat) {
 //        this.cat = cat;
 //    }   
-
     public Products getProductoSeleccionado() {
         return productoSeleccionado;
     }
@@ -199,11 +232,45 @@ public class ProductosController implements Serializable{
     public void setEnElCarrito(boolean enElCarrito) {
         this.enElCarrito = enElCarrito;
     }
-    
-    
-     public void addMessageError(String summary, String detail) {
+
+    public String getNuevaResenya() {
+        return nuevaResenya;
+    }
+
+    public void setNuevaResenya(String nuevaResenya) {
+        this.nuevaResenya = nuevaResenya;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+  
+    public void addMessageError(String summary, String detail) {
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, detail);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
-    
+
+    public List<String> completeArea(String query) {
+        List<String> results = new ArrayList<>();
+
+        if ("PrimeFaces".equals(query)) {
+            results.add("PrimeFaces Rocks!!!");
+            results.add("PrimeFaces has 100+ components.");
+            results.add("PrimeFaces is lightweight.");
+            results.add("PrimeFaces is easy to use.");
+            results.add("PrimeFaces is developed with passion!");
+        } else {
+            for (int i = 0; i < 10; i++) {
+                results.add(query + i);
+            }
+        }
+
+        return results;
+    }
+
 }
